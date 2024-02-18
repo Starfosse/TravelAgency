62d02583f0fc8cd844e1ce8b4c41f9c1743cadbe
Starfosse <alexis.bouleau92@yahoo.fr>
2024-02-18 21:52:50 +0100

    Update middleware.ts

--- travelagency/src/middleware.ts
+++ travelagency/src/middleware.ts
@@ -9,10 +9,5 @@ export default authMiddleware({
 })
 
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/",
-    "/(api|trpc)(.*)",
-    "/admin",
-  ],
+  matcher: [],
 }
